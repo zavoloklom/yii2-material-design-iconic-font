a4ffd71f0424f16c14d12eeb3ac8e784f474f28f
Sergey Kupletsky <s.kupletsky@gmail.com>
2016-04-27 14:23:58 +0300

    Create Project

---
+++ src/MaterialDesignIconicFontAsset.php
@@ -0,0 +1,20 @@
+<?php
+namespace zavoloklom\yii\material\font;
+
+/*
+ * Yii 2 asset from bower
+ *
+ * @package     yii2-material-design-iconic-font
+ * @link        https://github.com/zavoloklom/yii2-material-design-iconic-font GitHub Project
+ * @link        http://zavoloklom.github.io/material-design-iconic-font Material Design Iconic Font Homepage
+ * @copyright   Copyright &copy; Sergey Kupletsky, www.zavoloklom.com, 2016
+ * @license     MIT
+ * @author      Sergey Kupletsky <s.kupletsky@gmail.com>
+ */
+class MaterialDesignIconicFontAsset extends yii\web\AssetBundle
+{
+    public $sourcePath = '@bower/material-design-iconic-font/dist';
+    public $css = [
+        'css/material-design-iconic-font.min.css',
+    ];
+}
---
+++ src/MaterialDesignIconicFontCDNAsset.php
@@ -0,0 +1,19 @@
+<?php
+namespace zavoloklom\yii\material\font;
+
+/*
+ * Yii 2 asset from CDN
+ *
+ * @package     yii2-material-design-iconic-font
+ * @link        https://github.com/zavoloklom/yii2-material-design-iconic-font GitHub Project
+ * @link        http://zavoloklom.github.io/material-design-iconic-font Material Design Iconic Font Homepage
+ * @copyright   Copyright &copy; Sergey Kupletsky, www.zavoloklom.com, 2016
+ * @license     MIT
+ * @author      Sergey Kupletsky <s.kupletsky@gmail.com>
+ */
+class MaterialDesignIconicFontCDNAsset extends yii\web\AssetBundle
+{
+    public $css = [
+        '//cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css',
+    ];
+}
